67b709a78e89c2f0165d30c8c6e85e715c1df63f
rlpardoe <156867207+rlpardoe@users.noreply.github.com>
2024-04-15 23:22:45 -0500

    Finished Code

--- HW5/main/main.c
+++ HW5/main/main.c
@@ -1,91 +1,78 @@
 #include <stdio.h>
-
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
 
-
-/* Example code to talk to a BMP280 temperature and pressure sensor
-
-    NOTE: Ensure the device is capable of being driven at 3.3v NOT 5v. The Pico
-    GPIO (and therefore I2C) cannot be used at 5v.
-
-    You will need to use a level shifter on the I2C lines if you want to run the
-    board at 5v.
-
-    Connections on Raspberry Pi Pico board, other boards may vary.
-
-    GPIO PICO_DEFAULT_I2C_SDA_PIN (on Pico this is GP4 (pin 6)) -> SDA on BMP280
-    board
-    GPIO PICO_DEFAULT_I2C_SCK_PIN (on Pico this is GP5 (pin 7)) -> SCL on
-    BMP280 board
-    3.3v (pin 36) -> VCC on BMP280 board
-    GND (pin 38)  -> GND on BMP280 board
- */
-
- // device has default bus address of 0x76
-#define ADDR _u(0x76)
+#define ADDR _u(0x20) // & with last bit for read vs write -- handled by sdk I think
 
 // hardware registers
-#define REG_CONFIG _u(0xF5)
-#define REG_CTRL_MEAS _u(0xF4)
-#define REG_RESET _u(0xE0)
+#define IODIR _u(0x00)
+#define IOCON _u(0x05)
+#define GPIO _u(0x09)
+#define OLAT _u(0x0A)
 
 
-void bmp280_init() {
-    // use the "handheld device dynamic" optimal setting (see datasheet)
+void set_pins(unsigned char address, unsigned char value){
     uint8_t buf[2];
+    buf[0] = OLAT; // which SFR
+    buf[1] = value;
+    i2c_write_blocking(i2c_default, address, buf, 2, false);
 
-    // 500ms sampling time, x16 filter
-    const uint8_t reg_config_val = ((0x04 << 5) | (0x05 << 2)) & 0xFC;
-
-    // send register number followed by its corresponding value
-    buf[0] = REG_CONFIG;
-    buf[1] = reg_config_val;
-    i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
-
-    // osrs_t x1, osrs_p x4, normal mode operation
-    const uint8_t reg_ctrl_meas_val = (0x01 << 5) | (0x03 << 2) | (0x03);
-    buf[0] = REG_CTRL_MEAS;
-    buf[1] = reg_ctrl_meas_val;
-    i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+}
+unsigned char read_pins(unsigned char address, uint8_t *destination){
+    uint8_t buf = GPIO;
+    i2c_write_blocking(i2c_default, address, &buf, 1, true);
+    i2c_read_blocking(i2c_default,address,destination,1,false);
 }
 
-void bmp280_reset() {
-    // reset the device with the power-on-reset procedure
-    uint8_t buf[2] = { REG_RESET, 0xB6 };
-    i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+void chip_init() {
+    uint8_t buf1[2];
+    buf1[0] = IODIR;
+    buf1[1] = 0b0111111; // set GP7 to output, all the rest as input
+    i2c_write_blocking(i2c_default, ADDR, buf1, 2, false);
+    //outputs low on reset, set low anyway in case not rest since prev use
+    uint8_t buf2[2];
+    buf2[0] = OLAT;
+    buf2[1] = 0x00;
+    i2c_write_blocking(i2c_default, ADDR, buf1, 2, false);
 }
+
+
 
 int main() {
+
     stdio_init_all();
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    // useful information for picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-    bi_decl(bi_program_description("BMP280 I2C example for the Raspberry Pi Pico"));
-
-    printf("Hello, BMP280! Reading temperaure and pressure values from sensor...\n");
-
-    // I2C is "open drain", pull ups to keep signal high when no data is being sent
-    i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    
+    i2c_init(i2c_default, 100 * 1000); // 100 khz is fine
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C); // using default pins
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    /* Just breadbored some pull ups
+    /* Just breadboard some pull ups
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
     */
-    //Init chip
-    chip_init();
 
+    chip_init(); // set inputs v outputs and starting vals
 
-    sleep_ms(250); // sleep so that data polling and register update don't collide
+    uint8_t pin_status;
+
     while (1) {
-        //blink gp25 -- heartbeat if no heartbeat, youre stuck somewhere waiting, if led not blinking power reset, i2c probably got messed up
-        // read from gp0
-        //if gp0 high
-        //set gp7 high
-        //else 
-        // set gp7 low
+        //heartbeat
+        gpio_put(LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(100);
 
+        // read pins and update LED output
+        read_pins(ADDR, &pin_status) ;
+        if ((pin_status & 0x01) == 0){ // if gp0 is low, ie button is pressed, providing path to ground
+            set_pins(ADDR, 0x80); // set GP7 to high 0x80 = 1000 0000
+        }
+        else{
+            set_pins(ADDR, 0x00);//set GP7 back to low
+        }
     }
 }
